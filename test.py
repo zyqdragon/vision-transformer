b19bf426a93968ebf7603ae4b00f59fa9df40bc9
zyqdragon <zhangjdr@163.com>
2021-12-17 10:44:47 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -11,25 +11,50 @@ from PIL import Image
 from torchvision import datasets, transforms
 from tqdm.notebook import tqdm
 from model_vit import VIT_B16_224
+import os
+from linformer import Linformer
+from vit_pytorch.efficient import ViT
 
 # device = 'cuda'
 device = 'cpu'
 
-model=VIT_B16_224().to(device)
+efficient_transformer = Linformer(
+    dim=128,
+    seq_len=49+1,  # 7x7 patches + 1 cls-token
+    depth=12,
+    heads=8,
+    k=64
+)
+
+model = ViT(
+    dim=128,
+    image_size=224,
+    patch_size=32,
+    num_classes=2,
+    transformer=efficient_transformer,
+    channels=3,
+).to(device)
+
+# model=VIT_B16_224().to(device) # the model could be the ViT model
 
 if __name__ == '__main__':
     input_size = 224
     model=torch.load('./models/vit_model.pth')
+    model=torch.load('./models/vit_model_linear.pth')
     transform_valid = transforms.Compose([
        transforms.Resize((224, 224)),
        transforms.ToTensor()])
-    img = Image.open('./data/test/cat.1.jpg')
-   #  img = Image.open('./data/test/dog.1.jpg')
-    img_ = transform_valid(img).unsqueeze(0) #拓展维度
-    print(img_.shape)
-    # print(model.device)
-    # print(img_.device)
-    preds = model(img_.to('cuda:0')) # (1, 1000)
-    # print(preds)
-    kname, indices = torch.max(preds,1)
-    print("----kind=",indices)
+    img_list = os.listdir('./data/train')
+    for filename in img_list:
+       img = Image.open('./data/train/'+filename)
+       img_ = transform_valid(img).unsqueeze(0) #拓展维度
+       preds = model(img_.to('cuda:0')) # (1, 1000)
+       kname, indices = torch.max(preds,1)
+       print("-------img_name=",filename,"----kind=",indices)
+
+   #  single image is tested as follows:
+   #  img = Image.open('./data/test/cat.1.jpg')
+   #  img_ = transform_valid(img).unsqueeze(0) #拓展维度
+   #  preds = model(img_.to('cuda:0')) # (1, 1000)
+   #  kname, indices = torch.max(preds,1)
+   #  print("----kind=",indices)
